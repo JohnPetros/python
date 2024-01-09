a2436aaad928e843a5739377cda09e98fbb95144
João Pedro <joaopedro.nc@outlook.com>
2024-01-09 11:35:03 -0300

    ✨ feat: add class 03

---
+++ classes/class03.py
@@ -0,0 +1,18 @@
+# Class 03 - Modules
+
+from math import sqrt, ceil
+
+import random
+
+# import emoji
+
+number = int(input('Digite um número: '))
+squareRoot = sqrt(number)
+
+print(ceil(squareRoot))
+
+randomNumber = random.randint(1, 10)
+
+print(randomNumber)
+
+# print(emoji.emojize('Python is :thumbs_up:'))
