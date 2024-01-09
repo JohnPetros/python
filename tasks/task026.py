ae5398caafc61e965ec6b1f8edb8110c2e95ac83
João Pedro <joaopedro.nc@outlook.com>
2024-01-09 16:12:34 -0300

    ✨ feat: add task 026

---
+++ tasks/task026.py
@@ -0,0 +1,10 @@
+# Task 24 - Write a script that reads a phrase and displays
+# - How many times does the letter "A" appear in the phrase
+# - At what position does the letter "A" first appear in the phrase
+# - At what position does the letter "A" last appear in the phrase
+
+name = input('Type a phrase: ').strip().upper()
+
+print(f'The letter A appears {name.count("A")} times in the name')
+print(f'The first letter A appears at {name.find("A") + 1} position')
+print(f'The last letter A appears at {name.rfind("A") + 1} position')
