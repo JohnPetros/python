e42dc3e7b08126959b109182a31ee3d4d5cc7e23
João Pedro <joaopedro.nc@outlook.com>
2024-01-09 12:00:38 -0300

    ✨ feat: add task 015

---
+++ tasks/task015.py
@@ -0,0 +1,8 @@
+# Task 15 - Write a script that reads any float number and displays its integer part
+
+from math import trunc
+
+value = float(input('Type a float value: '))
+
+print(f'The value entered was {value} and its integer part is {trunc(value)}')
+
