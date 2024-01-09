87047ac99f781ae022997b0ecc6f7391fc3cfbee
João Pedro <joaopedro.nc@outlook.com>
2024-01-09 09:06:00 -0300

    ✨ feat: add task 009

---
+++ tasks/task009.py
@@ -0,0 +1,16 @@
+# Task 9 - Write a script that reads a integer and displays its mutiplication table.
+
+value = int(input('Write a value: '))
+
+print('-' * 11)
+print(f'{value} x 1 = {value * 1}')
+print(f'{value} x 2 = {value * 2}')
+print(f'{value} x 3 = {value * 3}')
+print(f'{value} x 4 = {value * 4}')
+print(f'{value} x 5 = {value * 5}')
+print(f'{value} x 6 = {value * 6}')
+print(f'{value} x 7 = {value * 7}')
+print(f'{value} x 8 = {value * 8}')
+print(f'{value} x 9 = {value * 9}')
+print(f'{value} x 10 = {value * 10}')
+print('-' * 11)
