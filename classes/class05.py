04081c23ab454d421d9b09a0c37279f659408a6b
João Pedro <joaopedro.nc@outlook.com>
2024-01-09 20:01:32 -0300

    ✨ feat: add class 05

---
+++ classes/class05.py
@@ -0,0 +1,10 @@
+# Class 05 - Colors in the Terminal 
+
+WHITE  = '\033[0m'  # white (normal)
+RED  = '\033[31m' # red
+GREEN  = '\033[32m' # green
+ORANGE  = '\033[33m' # orange
+BLUE  = '\033[34m' # blue
+PURPLE  = '\033[35m' # purple
+
+print(RED+"hello how are you"+WHITE)
