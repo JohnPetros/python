6c2b48d68209bc7f637d16b820a6b3221a98991f
João Pedro <joaopedro.nc@outlook.com>
2024-01-09 16:12:28 -0300

    ✨ feat: add task 025

---
+++ tasks/task025.py
@@ -0,0 +1,5 @@
+# Task 24 - Write a script that reads the name of the user and tells whether there is "silva" in the name
+
+name = input('What is your full name? ').strip().lower()
+
+print('silva' in name)
