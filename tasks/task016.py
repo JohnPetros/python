924a7a52ff5f8517cb9b598131927629b4d2224c
João Pedro <joaopedro.nc@outlook.com>
2024-01-09 12:00:48 -0300

    ✨ feat: add task 016

---
+++ tasks/task016.py
@@ -0,0 +1,11 @@
+# Task 16 - Write a script that reads the widths of the opposite and adjacent sides of a right-angled triangle, calculates, and displays the width of the hypotenuse.
+
+from math import pow, sqrt
+
+oppositeSide = float(input('The width of the opposite side: '))
+adjacentSide = float(input('The width of the adjacent side: '))
+
+hypotenuse = sqrt(pow(oppositeSide, 2) + pow(adjacentSide, 2))
+
+print(f'The hypotenuse is equal to {hypotenuse:.2f}')
+
