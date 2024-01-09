7344498f63001414282c3eab6275a06d8724aa39
João Pedro <joaopedro.nc@outlook.com>
2024-01-09 12:06:28 -0300

    ✨ feat: add task 017

---
+++ tasks/task017.py
@@ -0,0 +1,15 @@
+# Task 17 - Write a script that reads any angle and displays the value on its sine, cosine and tangent
+
+from math import sin, cos, tan, radians
+
+angle = float(input('Enter a angle: '))
+
+angleInradians = radians(angle)
+
+sine = sin(angleInradians)
+cosine = cos(angleInradians)
+tangent = tan(angleInradians)
+
+print(f'The sine is equal to {sine:.2f}')
+print(f'The cosine is equal to {cosine:.2f}')
+print(f'The tangent is equal to {tangent:.2f}')
