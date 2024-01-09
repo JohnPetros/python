4cbe2ebc8505b2c5205c4cf9224ed2b4a8a2e7a5
João Pedro <joaopedro.nc@outlook.com>
2024-01-09 08:19:53 -0300

    ✨ feat: add class 02

---
+++ classes/class02.py
@@ -0,0 +1,25 @@
+# Class 02 - Arithmetic Operators
+
+# Sum -> +
+print(5 + 5 + 2)
+
+# Subtraction -> -
+print(5 - 3)
+
+# Multiplication -> *
+print(5 * 3)
+
+# Division -> /
+print(10 / 5)
+
+# Division Quotient -> //
+print(5 // 2)
+
+# Division Remainder -> //
+print(5 % 2)
+
+# Exponatiation -> **
+print(8 ** 2)
+
+#  -> **
+print(8 ** 2)
