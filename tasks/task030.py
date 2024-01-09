69b3cbcb4f08c50537fc809b48ac9349be632750
João Pedro <joaopedro.nc@outlook.com>
2024-01-09 19:55:02 -0300

    ✨ feat: add task 030

---
+++ tasks/task030.py
@@ -0,0 +1,8 @@
+# Task 30 - Write a script that reads a number and informs whether the number is odd or even
+
+number = int(input('Tell me any number: '))
+
+if number % 2 == 0:
+  print(f'The number {number} is even')
+else:
+  print(f'The number {number} is odd')
