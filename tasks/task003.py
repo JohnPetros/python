b26d7eb51051f1be0ffda3592587a76aba346b4c
João Pedro <joaopedro.nc@outlook.com>
2024-01-09 08:19:03 -0300

    ✨ feat: add task 002

---
+++ tasks/task003.py
@@ -0,0 +1,6 @@
+# Task 3 - Write a script that reads two numbers and displays the sum between them.
+
+firstNumber = int(input('Write a number: '))
+secondNumber = int(input('Write another number: '))
+
+print(f'The sum between {firstNumber} and {secondNumber} is {firstNumber + secondNumber}!')
