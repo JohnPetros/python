c938d0f6921efc9ea9fe7b9e40fb079a482173ff
João Pedro <joaopedro.nc@outlook.com>
2024-01-09 13:02:00 -0300

    ✨ feat: add task 021

---
+++ tasks/task021.py
@@ -0,0 +1,13 @@
+# Task 18 - The same teacher from the previous task wants to randomly select the presentation order of the students' projects. Write a script that reads the names of the four students and displays the randomly chosen order.
+
+
+import pygame
+
+pygame.init()
+
+pygame.mixer.music.load('./assets/forest.mp3')
+
+pygame.mixer.music.play()
+
+pygame.event.wait()
+
