46acf577fd344a5002f6a6e797a36761659e25ed
João Pedro <joaopedro.nc@outlook.com>
2024-01-10 20:50:38 -0300

    ✨ feat: add class 07

---
+++ classes/class07.py
@@ -0,0 +1,16 @@
+# Class 07 - For Loop
+
+for counter in range(1, 6):
+    print(counter)
+
+print("END")
+
+for counter in range(0, 7, 2):
+    print(counter)
+
+print("END")
+
+for counter in range(6, 0, -1):
+    print(counter)
+
+print("END")
