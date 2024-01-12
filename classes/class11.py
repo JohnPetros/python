4fed8fc52e30eaea3e973652ee2307478e427387
João Pedro <joaopedro.nc@outlook.com>
2024-01-12 15:04:28 -0300

    ✨ feat: add  class 11

---
+++ classes/class11.py
@@ -0,0 +1,29 @@
+# Class 10 - Lists (Part 1)
+
+numbers = [2, 5, 9, 1]
+
+numbers[0] = 10
+
+print(numbers)
+
+numbers.append(7)
+
+print(numbers)
+
+numbers.sort()
+
+print(numbers)
+
+numbers.sort(reverse=True)
+
+print(numbers)
+
+numbers.insert(2, 0)
+
+numbers.pop()
+
+print(numbers)
+
+numbers.remove(0)
+
+print(numbers)
