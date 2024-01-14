ec901a418080f2c360597c69ac673f0cd2bd4a00
João Pedro <joaopedro.nc@outlook.com>
2024-01-14 08:33:51 -0300

    ✨ feat: add task 111

---
+++ tasks/task111.py
@@ -0,0 +1,3 @@
+# task 111 - Create a package called utils that contains two internal modules called money and data. Transfer all the functions used in the previous tasks into the first module and keep everything working.
+
+
